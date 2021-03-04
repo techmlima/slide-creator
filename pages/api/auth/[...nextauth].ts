ae5a4e9cbf2ef6b40f1814a11f27f9ebb1b1c376
marcosbl <marcosbl@stj.jus.br>
2021-03-04 16:07:18 -0300

    remover login por github

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,10 +9,6 @@ export default authHandler
 
 const options = {
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
