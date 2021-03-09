e19a2ea271dd03beaf93c65d325dbe2968749286
marcosbl <marcosbl@stj.jus.br>
2021-03-09 17:05:12 -0300

    mensagem de lembrete para modal pdf

--- components/Modal/ModalPDFView.tsx
+++ components/Modal/ModalPDFView.tsx
@@ -10,6 +10,7 @@ import TooltipElement from "../TooltipElement";
 const colorsEnum = require("../../util/colors");
 
 const ModalPDFView: React.FC<{ show, onHide, textsSelected: TextSourceProps[], changeOrderList }> = ({ show, onHide, textsSelected, changeOrderList }) => {
+    //TODO: BUSCAR PREFERÊNCIAS DO BANCO DE DADOS
     const [showConfig, setShowConfig] = useState(false);
     const [pdfStyleSheet, setPdfStyleSheet] = useState({
         fontSize: 17,
@@ -19,104 +20,102 @@ const ModalPDFView: React.FC<{ show, onHide, textsSelected: TextSourceProps[], c
     });
 
     return (
-        <>
-            <Modal
-                onHide={onHide}
-                show={show}
-                dialogClassName="modal-custom"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Pré-visualizar PDF
+        <Modal
+            onHide={onHide}
+            show={show}
+            dialogClassName="modal-custom"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Pré-visualizar PDF
                     </Modal.Title>
-
-                    <TooltipElement keyName='topConfigPDF' placement='top' text='Configurações do PDF'
-                        component={(
-                            <Button
-                                variant='info'
-                                className='ml-2'
-                                onClick={() => setShowConfig(!showConfig)}
-                                aria-controls="collapse-config-pdf"
-                                aria-expanded={showConfig}
-                            >
-                                <Gear />
-                            </Button>
-                        )}>
-                    </TooltipElement>
-                </Modal.Header>
-                <Modal.Body>
-                    <Collapse in={showConfig}>
-                        <div id="collapse-config-pdf">
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formTamanho">
-                                    <Form.Label>Tamanho letra</Form.Label>
-                                    <Form.Control type="number" min="1" max="60" value={pdfStyleSheet.fontSize}
-                                        onChange={(e) => {
-                                            const num = Number(e.target.value);
-                                            if (num >= 1 && num <= 60) {
-                                                setPdfStyleSheet({ ...pdfStyleSheet, fontSize: num })
-                                            }
-                                        }} />
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formCor">
-                                    <Form.Label>Cor letra</Form.Label>
-                                    <Form.Control as="select" custom value={pdfStyleSheet.fontColor}
-                                        onChange={(e) =>
-                                            setPdfStyleSheet({ ...pdfStyleSheet, fontColor: e.target.value })
-                                        }>
-                                        {colorsEnum.values().map((color, index) => (
-                                            <option key={`listCorLetra${index}`} value={color}>{colorsEnum.getNameByEnum(color)}</option>
-                                        ))}
-                                    </Form.Control>
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formFundo">
-                                    <Form.Label>Cor do fundo</Form.Label>
-                                    <Form.Control as="select" custom value={pdfStyleSheet.backgroundColor}
-                                        onChange={(e) =>
-                                            setPdfStyleSheet({ ...pdfStyleSheet, backgroundColor: e.target.value })
-                                        }>
-                                        {colorsEnum.values().map((color, index) => (
-                                            <option key={`listFundo${index}`} value={color}>{colorsEnum.getNameByEnum(color)}</option>
-                                        ))}
-                                    </Form.Control>
-                                </Form.Group>
-                            </Form.Row>
-                        </div>
-                    </Collapse>
-
-                    <div className="row h-100">
-                        <div className="col-8">
-                            <PDFViewer>
-                                <MyDocument textSource={textsSelected} pdfStyleSheet={pdfStyleSheet} />
-                            </PDFViewer>
-                        </div>
-                        <div className="col border-left">
-                            <div className="row">
-                                <div className="col">
-                                    <DragAndDrop textsSource={textsSelected} changeOrderList={changeOrderList} />
-                                </div>
-                            </div>
-
-                        </div>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="row h-100">
+                    <div className="col-8">
+                        <PDFViewer>
+                            <MyDocument textSource={textsSelected} pdfStyleSheet={pdfStyleSheet} />
+                        </PDFViewer>
                     </div>
+                    <div className="col border-left">
+                        <div className="row">
+                            <div className="col">
+                                <TooltipElement keyName='topConfigPDF' placement='top' text='Configurações do PDF'
+                                    component={(
+                                        <Button
+                                            variant='info'
+                                            className='ml-2'
+                                            onClick={() => setShowConfig(!showConfig)}
+                                            aria-controls="collapse-config-pdf"
+                                            aria-expanded={showConfig}
+                                        >
+                                            <Gear />
+                                        </Button>
+                                    )}>
+                                </TooltipElement>
 
-                </Modal.Body>
-                <Modal.Footer>
-                    <PDFDownloadLink className="ml-2" document={<MyDocument textSource={textsSelected} pdfStyleSheet={pdfStyleSheet} />} fileName="Documento.pdf">
-                        {({ blob, url, loading, error }) => (loading ? '...' :
-                            <div className='btn btn-info' >
-                                Download PDF <Download className="mb-1" />
+                                <Collapse in={showConfig}>
+                                    <div id="collapse-config-pdf">
+                                        <Form.Row>
+                                            <Form.Group as={Col} controlId="formTamanho">
+                                                <Form.Label>Tamanho letra</Form.Label>
+                                                <Form.Control type="number" min="1" max="60" value={pdfStyleSheet.fontSize}
+                                                    onChange={(e) => {
+                                                        const num = Number(e.target.value);
+                                                        if (num >= 1 && num <= 60) {
+                                                            setPdfStyleSheet({ ...pdfStyleSheet, fontSize: num })
+                                                        }
+                                                    }} />
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} controlId="formCor">
+                                                <Form.Label>Cor letra</Form.Label>
+                                                <Form.Control as="select" custom value={pdfStyleSheet.fontColor}
+                                                    onChange={(e) =>
+                                                        setPdfStyleSheet({ ...pdfStyleSheet, fontColor: e.target.value })
+                                                    }>
+                                                    {colorsEnum.values().map((color, index) => (
+                                                        <option key={`listCorLetra${index}`} value={color}>{colorsEnum.getNameByEnum(color)}</option>
+                                                    ))}
+                                                </Form.Control>
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} controlId="formFundo">
+                                                <Form.Label>Cor do fundo</Form.Label>
+                                                <Form.Control as="select" custom value={pdfStyleSheet.backgroundColor}
+                                                    onChange={(e) =>
+                                                        setPdfStyleSheet({ ...pdfStyleSheet, backgroundColor: e.target.value })
+                                                    }>
+                                                    {colorsEnum.values().map((color, index) => (
+                                                        <option key={`listFundo${index}`} value={color}>{colorsEnum.getNameByEnum(color)}</option>
+                                                    ))}
+                                                </Form.Control>
+                                            </Form.Group>
+                                        </Form.Row>
+                                    </div>
+                                </Collapse>
+
+                                <DragAndDrop textsSource={textsSelected} changeOrderList={changeOrderList} />
                             </div>
-                        )}
-                    </PDFDownloadLink>
-                    <Button onClick={onHide}>Fechar</Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+                        </div>
+
+                    </div>
+                </div>
+
+            </Modal.Body>
+            <Modal.Footer>
+                <PDFDownloadLink className="ml-2" document={<MyDocument textSource={textsSelected} pdfStyleSheet={pdfStyleSheet} />} fileName="Documento.pdf">
+                    {({ blob, url, loading, error }) => (loading ? '...' :
+                        <div className='btn btn-info' >
+                            Download PDF <Download className="mb-1" />
+                        </div>
+                    )}
+                </PDFDownloadLink>
+                <Button onClick={onHide}>Fechar</Button>
+            </Modal.Footer>
+        </Modal>
     );
 }
 
